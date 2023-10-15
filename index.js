8ee3a273e6da58d0a57b17c36497ff7819e56972
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2023-10-15 14:35:13 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const auth = require("json-server-auth");
+
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 9000; //  chose port from here like 8080, 3001
+
+server.use(middlewares);
+server.use(auth);
+server.use(router);
+
+server.db = router.db;
+// Set default middlewares (logger, static, cors, etc.)
+server.use(middlewares);
+server.use(jsonServer.bodyParser); // Required to parse POST requests
+
+server.listen(port);
